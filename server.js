e2a498bcd556b1148f0380149ae4147994c391a5
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-12-23 17:16:22 -0500

    Added restaurant data delivery
    The server provides advertising data (The restaurant banners, names, descriptions, etc...) to the client for use on the main page. This is currently stored as JSON and will be updated when the database is ready.

--- server.js
+++ server.js
@@ -3,16 +3,79 @@ var app = express();
 var server = require("http").Server(app);
 
 //you never know when it might come in handy :)
-var io = require("socket.io");
+var io = require("socket.io")(server);
 
 
-app.use(express.static("/static-files"));
+
+//keeping it as JSON for now; will be moved to database once that becomes available
+var restaurantData = [{
+
+  "name":"Dogs to Go",
+  "avgWait":"4 minutes",
+  "image":"/dogs-to-go.jpg",
+  "description":"Eat some dogs! East some dogs! Eat some dogs!"
+
+}, {
+
+  "name":"Easy As Pie Diner",
+  "avgWait":"18 minutes",
+  "image":"/easy-as-pie-diner.jpg",
+  "description": "Enjoy Gordon Ramsay's signature cuisine in this premium dining experience."
+
+},{
+
+  "name":"Forever Cool Ice Cream",
+  "avgWait":"8 minutes",
+  "image":"/foreveer-cool-ice-cream.jpg",
+  "description":"Enjoy ice cream that is forever cool in a definitely non-ambiguous way."
+
+},{
+
+  "name":"Chickens-R-Us",
+  "avgWait":"10 minutes",
+  "image":"/chickens-r-us.jpg",
+  "description":"CHICKENS ARE US. WE ARE THE CHICKENS."
+
+},{
+
+  "name":"Chuck Wagon",
+  "avgWait": "17 minutes",
+  "image":"/chuck-wagon.jpg",
+  "description":"A classic American diner. If you eat enough, we might even let you ride in Chuck's wagon..."
+
+}];
+
+io.on("connection", function(socket){
+
+
+  socket.on("getAdData", function(msg){
+    socket.emit("getAdData", restaurantData);
+  });
+
+
+});
+
+
+app.use(express.static("static"));
 
 
 app.get("/", function(req, res){
 
   //replace directory with actual value of client file
-  res.sendFile('reservation.html');
+  res.sendFile(__dirname + "/static/test.html");
+
+});
+
+app.get("/d/socket.io", function(req, res){
+
+  res.sendFile(__dirname + "/node_modules/socket.io/client-dist/socket.io.js");
+
+});
+
+app.get("/:place", function(req, res){
+
+  //replace directory with actual value of client file
+  res.sendFile(__dirname + "/static/" + req.param.place);
 
 });
 
@@ -23,8 +86,6 @@ app.post("/something", function(req, res){
   //handle forms in these thingies
 
 });
-
-
 
 
 
