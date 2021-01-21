5c6dea5a2819efaa58432d9664f4c254302f7c41
pschwendy <peterschwende@gmail.com>
2021-01-21 01:20:14 -0500

    more queries

--- queries.js
+++ queries.js
@@ -82,6 +82,56 @@ class Queries{
             }
         });
     } /* reserve */
+
+    // reservation function
+    // deletes reservation from reservations table
+    // input: attraction -> name of attraction reserved
+    // input: account_id -> user id of logged in account 
+    // input: num_people -> number of people in reservation
+    // input: res_time -> time of reservation
+    // input: res_date -> date of reservation
+    delete_reservation(attraction, account_id, num_people, res_time, res_date) {
+        let sql = "DELETE FROM reservations WHERE attractionName=$attraction AND numPeople=$num_people AND time=$time AND date=$date AND user=$user";
+        db.run(sql, {
+            $attraction: attraction,
+            $num_people: num_people,
+            $time: res_time,
+            $date: res_date,
+            $user: account_id,
+        }, (err) => {
+            if(err) {
+                throw(err);
+            }
+        });
+    } /* delete_reservation */
+
+    // reservation function
+    // deletes reservation from reservations table
+    // input: attraction -> name of attraction reserved
+    // input: account_id -> user id of logged in account 
+    // input: old_num_people -> original number of people in reservation
+    // input: old_time -> original time of reservation
+    // input: old_date -> original date of reservation
+    // input: new_num_people -> updated number of people in reservation
+    // input: new_time -> updated time of reservation
+    // input: new_date -> updated date of reservation
+    update_reservation(attraction, account_id, old_num_people, old_time, old_date, new_num_people, new_time, new_date) {
+        let sql = "UPDATE reservations SET numPeople=$new_new_people, time=$new_time, date=$new_date WHERE attractionName=$attraction AND numPeople=$old_num_people AND time=$old_time AND date=$old_date AND user=$user";
+        db.run(sql, {
+            $attraction: attraction,
+            $old_num_people: old_num_people,
+            $old_time: old_time,
+            $old_date: old_date,
+            $user: account_id,
+            $new_num_people: new_num_people,
+            $new_time: new_time,
+            $new_date: new_date,
+        }, (err) => {
+            if(err) {
+                throw(err);
+            }
+        });
+    } /* update_reservation */
 }
 
 export { Queries };
