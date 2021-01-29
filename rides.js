10ba9315836f9cb366be2cf11d5afd7f11f90ad8
Aarnav Unadkat <aarnavkalpesh@gmail.com>
2021-01-28 23:49:32 -0500

    aarnav definitely made this commit

---
+++ rides.js
@@ -0,0 +1,18 @@
+/*window.onload = function(){
+
+
+    $(".text-pic").hide();
+  
+}*/
+
+$("#rollercoasterbutton").on("click", () => {
+    $(".text-pic").hide();
+    $("#rollercoaser").show();
+   console.log("button pressed");
+ })
+ 
+ $("#rando").on("click", () => {
+    $(".text-pic").hide();
+    $("#rollercoaser").show();
+   console.log("button pressed");
+ })
